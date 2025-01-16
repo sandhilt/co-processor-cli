775b9c92be335da5c550ad47fbd67de64185bc74
Idogwu Chinonso <idogwuchinonso1@gmail.com>
2025-01-16 01:07:15 +0100

    mod: updated deploy function to deploy solidity programs via cli

--- src/helpers/helpers.rs
+++ src/helpers/helpers.rs
@@ -1,10 +1,30 @@
+use crate::commands::deploy::deploy_contract;
+use crate::commands::register::{devnet_register, mainnet_register};
 use colored::Colorize;
+use enum_iterator::{all, Sequence};
 use std::env;
 use std::fs;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 use std::process::{Command, Stdio};
 use std::{thread, time};
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Debug, Sequence)]
+enum DeploymentOptions {
+    Devnet,
+    Testnet,
+    Mainnet,
+}
+
+impl DeploymentOptions {
+    fn to_string(&self) -> String {
+        match self {
+            DeploymentOptions::Devnet => "Devnet".to_string(),
+            DeploymentOptions::Testnet => "Testnet".to_string(),
+            DeploymentOptions::Mainnet => "Mainnet".to_string(),
+        }
+    }
+}
 
 /// @notice Helper Function to check all the required dependencies are installed
 /// @returns a boolean value indicating whether or not all dependencies are installed
@@ -208,4 +228,87 @@ pub fn check_deploymet_args(
         reject = true;
     }
     return reject;
+}
+
+pub fn check_registration_environment(network: String, email: String) {
+    let mut environment: Option<DeploymentOptions> = None;
+
+    for option in all::<DeploymentOptions>().collect::<Vec<_>>() {
+        if network.to_lowercase() == option.to_string().to_lowercase() {
+            environment = Some(option);
+        }
+    }
+
+    if environment.is_none() {
+        println!(
+            "{}",
+            "Invalid network environment, please select either, devnet, mainnet, or testnet".red()
+        );
+        return;
+    }
+
+    if let Some(deployment_env) = environment {
+        match deployment_env {
+            DeploymentOptions::Devnet => {
+                devnet_register(email);
+            }
+            DeploymentOptions::Testnet => {
+                println!("Sorry Testnet integration is not available at the moment!!",);
+            }
+            DeploymentOptions::Mainnet => {
+                mainnet_register(email);
+            }
+        }
+    }
+}
+
+pub fn check_deployment_environment(
+    network: String,
+    private_key: Option<String>,
+    rpc: Option<String>,
+    constructor_args: Option<Vec<String>>,
+    contract_name: String,
+) {
+    match network.to_lowercase().as_str() {
+        "mainnet" => {
+            if check_deploymet_args(&network, private_key.clone(), rpc.clone()) {
+                return;
+            } else {
+                deploy_contract(
+                    private_key.unwrap(),
+                    rpc.unwrap(),
+                    constructor_args,
+                    contract_name,
+                );
+            }
+        }
+        "testnet" => {
+            if check_deploymet_args(&network, private_key.clone(), rpc.clone()) {
+                return;
+            } else {
+                deploy_contract(
+                    private_key.unwrap(),
+                    rpc.unwrap(),
+                    constructor_args,
+                    contract_name,
+                );
+            }
+        }
+        "devnet" => {
+            deploy_contract(
+                String::from("ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"),
+                String::from("http://127.0.0.1:8545"),
+                constructor_args,
+                contract_name,
+            );
+        }
+        _ => {
+            println!(
+                "{}",
+                "Invalid network environment, please select either, devnet, mainnet, or testnet"
+                    .red()
+            );
+            return;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 mod commands;
 mod helpers;
 use crate::commands::create::create;
-use crate::commands::deploy::deploy_contract;
-use crate::commands::register::{devnet_register, mainnet_register};
 use crate::helpers::helpers::check_dependencies_installed;
 use clap::{Parser, Subcommand};
-use colored::Colorize;
-use enum_iterator::{all, Sequence};
-use helpers::helpers::check_deploymet_args;
+use helpers::helpers::{check_deployment_environment, check_registration_environment};
 use std::error::Error;
 
 /// A CLI tool to interact with Web3.Storage
@@ -51,6 +47,9 @@ enum Commands {
         long_about = "Deploy the solidity code for your coprocessor program to any network of choice, by running the default deploy script (Deploy.s.sol)"
     )]
     Deploy {
+        #[arg(short, long, help = "Name of your contract file")]
+        contract_name: String,
+
         #[arg(
             short,
             long,
@@ -63,24 +62,16 @@ enum Commands {
 
         #[arg(short, long, help = "RPC for deploying to network of choice")]
         rpc: Option<String>,
+
+        #[arg(
+        short = 'a',
+        long,
+        help = "Constructor arguments to pass to the contract",
+        num_args = 0..,
+        value_delimiter = ' '
+        )]
+        constructor_args: Option<Vec<String>>,
     },
-}
-
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Debug, Sequence)]
-enum DeploymentOptions {
-    Devnet,
-    Testnet,
-    Mainnet,
-}
-
-impl DeploymentOptions {
-    fn to_string(&self) -> String {
-        match self {
-            DeploymentOptions::Devnet => "Devnet".to_string(),
-            DeploymentOptions::Testnet => "Testnet".to_string(),
-            DeploymentOptions::Mainnet => "Mainnet".to_string(),
-        }
-    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -100,78 +91,22 @@ fn main() -> Result<(), Box<dyn Error>> {
                 Ok(())
             }
             Commands::Deploy {
+                contract_name,
                 network,
                 private_key,
                 rpc,
+                constructor_args,
             } => {
-                check_deployment_environment(network, private_key, rpc);
+                // let contract_path = check_contract_exists(&file_name);
+                check_deployment_environment(
+                    network,
+                    private_key,
+                    rpc,
+                    constructor_args,
+                    contract_name,
+                );
                 Ok(())
             }
         },
-    }
-}
-
-fn check_registration_environment(network: String, email: String) {
-    let mut environment: Option<DeploymentOptions> = None;
-
-    for option in all::<DeploymentOptions>().collect::<Vec<_>>() {
-        if network.to_lowercase() == option.to_string().to_lowercase() {
-            environment = Some(option);
-        }
-    }
-
-    if environment.is_none() {
-        println!(
-            "{}",
-            "Invalid network environment, please select either, devnet, mainnet, or testnet".red()
-        );
-        return;
-    }
-
-    if let Some(deployment_env) = environment {
-        match deployment_env {
-            DeploymentOptions::Devnet => {
-                devnet_register(email);
-            }
-            DeploymentOptions::Testnet => {
-                println!("Sorry Testnet integration is not available at the moment!!",);
-            }
-            DeploymentOptions::Mainnet => {
-                mainnet_register(email);
-            }
-        }
-    }
-}
-
-fn check_deployment_environment(network: String, private_key: Option<String>, rpc: Option<String>) {
-    match network.to_lowercase().as_str() {
-        "mainnet" => {
-            if check_deploymet_args(&network, private_key.clone(), rpc.clone()) {
-                return;
-            } else {
-                deploy_contract(private_key.unwrap(), rpc.unwrap());
-            }
-        }
-        "testnet" => {
-            if check_deploymet_args(&network, private_key.clone(), rpc.clone()) {
-                return;
-            } else {
-                deploy_contract(private_key.unwrap(), rpc.unwrap());
-            }
-        }
-        "devnet" => {
-            deploy_contract(
-                String::from("ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"),
-                String::from("http://127.0.0.1:8545"),
-            );
-        }
-        _ => {
-            println!(
-                "{}",
-                "Invalid network environment, please select either, devnet, mainnet, or testnet"
-                    .red()
-            );
-            return;
-        }
     }
 }
